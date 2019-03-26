d1c09e2caed550a6869ef8ddce073371462a2b95
Eric <460181594@qq.com>
2019-03-26 21:46:58 +0800

    Controller

---
+++ src/test/java/com/dx/ss/demo/TwinsBufferDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dx.ss.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwinsBufferDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
