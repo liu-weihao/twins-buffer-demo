d57f6327b9cd25cf6debe768d0cf8119c65ef805
Eric <460181594@qq.com>
2019-03-26 21:45:54 +0800

    new project

---
+++ src/main/java/com/dx/ss/demo/TwinsBufferDemoApplication.java
@@ -0,0 +1,31 @@
+package com.dx.ss.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.InetAddress;
+
+@Slf4j
+@SpringBootApplication
+public class TwinsBufferDemoApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        ConfigurableApplicationContext context = SpringApplication.run(TwinsBufferDemoApplication.class, args);
+        ConfigurableEnvironment env = context.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t" +
+                        "----------------------------------------------------------",
+
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
+
+}
